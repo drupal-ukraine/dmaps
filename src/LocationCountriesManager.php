8793199357d8d39a7675a641ef6621d8117a6b9b
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-01-13 23:59:02 +0300

    Add more functions to LocationCountriesManager

--- src/LocationCountriesManager.php
+++ src/LocationCountriesManager.php
@@ -26,6 +26,13 @@ class LocationCountriesManager implements LocationCountriesManagerInterface {
    * @inheritdoc
    */
   public function getSupportedList() {
+    $supported_countries = &drupal_static(__FUNCTION__, array());
+
+    // If this function has already been called this request, we can avoid a DB hit.
+    if (!empty($supported_countries)) {
+      return $supported_countries;
+    }
+
     // Try first to load from cache, it's much faster than the scan below.
     if ($cache = $this->cache->get('location:supported-countries')) {
       $supported_countries = $cache->data;
@@ -33,7 +40,7 @@ class LocationCountriesManager implements LocationCountriesManagerInterface {
     else {
       // '<ISO two-letter code>' => '<English name for country>'
       $iso_list = static::getIso3166List();
-      $path = drupal_get_path('module', 'dmaps') . '/files/supported_countries/';
+      $path = DRUPAL_ROOT . '/' . drupal_get_path('module', 'dmaps') . '/files/supported_countries/location.';
       foreach ($iso_list as $cc => $name) {
         if (file_exists($path . $cc . '.inc')) {
           $supported_countries[$cc] = $name;
@@ -41,6 +48,7 @@ class LocationCountriesManager implements LocationCountriesManagerInterface {
       }
       $this->cache->set('location:supported-countries', $supported_countries);
     }
+
     return $supported_countries;
   }
 
@@ -55,15 +63,93 @@ class LocationCountriesManager implements LocationCountriesManagerInterface {
   /**
    * Get list of ISO 3166-1 alpha-2 countries.
    *
+   * @param bool $upper
+   *   Indicate return codes in upper case or not.
+   *
    * @return array
    *   List of all countries in format country-code => country-name.
    */
-  public static function getIso3166List() {
-    $countries = \Drupal::service('country_manager')->getList();
-
-    // Module uses lower-case ISO 3166-1 alpha2 codes, so we need to convert.
-    $countries = array_change_key_case($countries, CASE_LOWER);
+  public static function getIso3166List($upper = FALSE) {
+    // Statically cache a version of the core Drupal list of countries
+    // with lower case country codes for use by this module.
+    $countries = &drupal_static(__FUNCTION__);
+    if ($upper) {
+      // Drupal core stores ISO 3166-1 alpha2 codes in upper case, as
+      // per the ISO standard.
+      return \Drupal::service('country_manager')->getList();
+    }
+    elseif (!isset($countries)) {
+      $countries = \Drupal::service('country_manager')->getList();
+      // Module uses lower-case ISO 3166-1 alpha2 codes, so we need to convert.
+      $countries = array_change_key_case($countries, CASE_LOWER);
+    }
 
     return $countries;
+  }
+
+  /**
+   * Load support for a country.
+   *
+   * This function will load support for a country identified by its two-letter ISO code.
+   *
+   * @param string $country
+   *   Two-letter ISO code for country.
+   *
+   * @return bool
+   *   TRUE if the file was found and loaded, FALSE otherwise.
+   */
+  public static function locationLoadCountry($country) {
+    static::locationStandardizeCountryCode($country);
+
+    $file = DRUPAL_ROOT . '/' . drupal_get_path('module', 'dmaps') . '/files/supported_countries/location.' . $country . '.inc';
+    if (file_exists($file)) {
+      include_once $file;
+
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Canonicalize a country code.
+   *
+   * @param string $country
+   *   Two-letter ISO code for country.
+   *
+   * @return bool
+   */
+  public static function locationStandardizeCountryCode(&$country) {
+    $country = trim($country);
+    // Double check the validity of this validity check.
+    if (!ctype_alpha($country) || strlen($country) != 2) {
+      $country = 'xx';
+
+      return FALSE;
+    }
+    else {
+      $country = strtolower($country);
+
+      return TRUE;
+    }
+  }
+
+  /**
+   * Get the translated name of a country code.
+   *
+   * @param string $country
+   *   Two-letter ISO code for country.
+   *
+   * @return string
+   */
+  public function locationCountryName($country = 'us') {
+    static::locationStandardizeCountryCode($country);
+    $countries = static::getIso3166List();
+    if (isset($countries[$country])) {
+
+      return $countries[$country];
+    }
+
+    return '';
   }
 }
