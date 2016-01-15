968e03611e8e7172ad7e6b3444dfd01936f7f5f6
Dmitry Drozdik <dmitriydrozdik@gmail.com>
2016-01-15 12:05:04 +0300

    Add modifications to test MainSettingsFormTest

--- d8/modules/custom/dmaps/src/Tests/Form/MainSettingsFormTest.php
+++ d8/modules/custom/dmaps/src/Tests/Form/MainSettingsFormTest.php
@@ -41,7 +41,15 @@ class MainSettingsFormTest extends WebTestBase {
    */
   function testSettingsForm() {
     $this->drupalGet('/admin/config/content/location');
-    $this->assertText('Default country selection', 'Default country selection option is present');
+    $this->assertFieldByName('location_default_country', NULL, 'The field "Default country selection" is available.');
+    $this->assertFieldByName('location_display_location', NULL, 'The field "Toggle location display" is available.');
+    $this->assertFieldByName('location_use_province_abbreviation', NULL, 'The field "Province display" is available.');
+    // @todo add validation for field location_usegmap, it should be disabled if module gmap is not installed.
+    $this->assertFieldByName('location_usegmap', NULL, 'The field "Use a Google Map to set latitude and longitude" is available.');
+    $this->assertFieldByName('location_locpick_macro', NULL, 'The field "Location chooser macro" is available.');
+    $this->assertFieldByName('location_jit_geocoding', NULL, 'The field "Enable JIT geocoding" is available.');
+    $this->assertFieldByName('location_maplink_external', NULL, 'The field "Open map link in new window" is available.');
+    $this->assertFieldByName('location_maplink_external_method', NULL, 'The field "Open in new window method" is available.');
     $this->drupalPostForm('/admin/config/content/location', [], t('Save configuration'));
     $this->assertText(t('The configuration options have been saved.'));
   }
