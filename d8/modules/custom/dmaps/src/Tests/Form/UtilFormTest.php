be9141719b62ff39cd44124fee6ca533966768af
afi13 <dimoncds@gmail.com>
2016-01-28 11:53:21 +0300

    Test for testing utilities form

--- d8/modules/custom/dmaps/src/Tests/Form/UtilFormTest.php
+++ d8/modules/custom/dmaps/src/Tests/Form/UtilFormTest.php
@@ -1,14 +1,56 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: dmitry
- * Date: 28.01.16
- * Time: 11:15
+ * Contains \Drupal\dmaps\Tests\Form\UtilFormTest.
  */
 
 namespace Drupal\dmaps\Tests\Form;
 
+use Drupal\simpletest\WebTestBase;
 
-class UtilFormTest {
+/**
+ * Test Dmaps utils form
+ *
+ * @group Form
+ */
+class UtilFormTest extends WebTestBase {
 
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('dmaps');
+
+
+  /**
+   * A user that has permission to administer site configuration.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $web_user;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->web_user = $this->drupalCreateUser(['administer site configuration']);
+    $this->drupalLogin($this->web_user);
+  }
+
+  /**
+   * Testing Utils form for Dmaps module.
+   */
+  function testUtilsForm() {
+    $this->drupalGet('/admin/config/content/location/util');
+    $this->assertText(t('Clear province cache'));
+    $this->assertText(t('If you have modified location.xx.inc files, you will need to clear the province cache to get Location to recognize the modifications.'));
+    $this->assertText(t('Clear supported country list'));
+    $this->assertText(t('If you have added support for a new country, you will need to clear the supported country list to get Location to recognize the modifications.'));
+    $this->drupalPostForm('/admin/config/content/location/util', [], t('Clear province cache'));
+    $this->assertText(t('Location province cache cleared.'));
+    $this->drupalPostForm('/admin/config/content/location/util', [], t('Clear supported country list'));
+    $this->assertText(t('Location supported country list cleared.'));
+  }
 }
